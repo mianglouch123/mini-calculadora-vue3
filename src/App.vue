bb0f6da58f6a18eb33273a2f245eb3471c70f0fe
mianglouch123 <miangpermer@gmail.com>
2023-01-25 12:55:15 -0500

    commit proyecto

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+
+<simpleCalculator />
+
+
+
+
+</template>
+
+
+
+<script >
+import simpleCalculator from './components/simpleCalculator.vue'
+export default{
+  name:'App',
+  components:{
+    simpleCalculator
+  }
+}
+
+
+</script>
+
+
+
+
+
+
+
